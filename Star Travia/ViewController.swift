278d2e2f9145f3a042b6697d65bdbba397bd1871
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-09-18 21:38:48 +0530

    Initial Commit

---
+++ Star Travia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Travia
+//
+//  Created by Ajinkya Sonar on 18/09/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
